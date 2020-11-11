6e986fb75b77eeaf78edcab8ceb8558be8e797c7
Seba123XDHD <sebiquirozbad@gmail.com>
2020-11-10 22:16:28 -0300

    comportamiento posible de enemigo

---
+++ Assets/EnemyShmupFighter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShmupFighter : MonoBehaviour
+{
+    public Vector3 startRotation=  new Vector3(0, 0, 180);
+    private void OnEnable()
+    {
+        transform.localEulerAngles = startRotation;
+    }
+    // Start is called before the first frame update
+    public void Destroyenemy()
+    {
+        gameObject.SetActive(false);
+    }
+}
