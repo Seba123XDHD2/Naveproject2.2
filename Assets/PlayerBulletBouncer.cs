485317a71b8c81e5f6e849656109cc525ea40871
Seba123XDHD <sebiquirozbad@gmail.com>
2020-11-11 15:41:38 -0300

    Bullet rebote

---
+++ Assets/PlayerBulletBouncer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletBouncer : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public string bounceTargetTag;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if(collision.gameObject.CompareTag(bounceTargetTag))
+        {
+
+            Vector2 reflectedVelocity = Vector2.Reflect(rb.velocity, collision.GetContact(0).normal);
+
+            rb.velocity = reflectedVelocity;
+
+        }
+    }
+}
