221ac894c37e15a9fe249a8e3d211c6b7d617729
Seba123XDHD <sebiquirozbad@gmail.com>
2020-12-01 19:15:10 -0300

    cambi cosas del menu

---
+++ Assets/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public Text Scoretext;
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void UpdateScore()
+    {
+        Scoretext.text = "Score: " + score;
+    }
+    public void AddScore(int value)
+    {
+        score += value;
+        UpdateScore();
+    }
+}
