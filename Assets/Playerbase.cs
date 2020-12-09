1e216264bb7f6a23c07d47420f87f122067407e2
Seba123XDHD <sebiquirozbad@gmail.com>
2020-12-09 19:08:16 -0300

    ya muere jugador y respawnea 3 veces

--- Assets/Playerbase.cs
+++ Assets/Playerbase.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using Taller;
 public class Playerbase : MonoBehaviour
 {
     public void destroyShip()
     {
         Playerdeathpool.Instance?.GetPooledObject(transform.position);
         gameObject.SetActive(false);
+        GameManager.Instance?.PlayerDead();
     }
     
     
